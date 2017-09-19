70c64a71f97eb393fa6830bb00d3e4256356c772
gigihpc <gigih.math.its@gmail.com>
2017-09-19 18:15:56 +0700

    add login n register

---
+++ vuetify_tamplate/webpack_advanced/src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-form ref="form">
+      <v-layout justify-center>
+    <v-flex xs12 sm10 md8 lg6>
+    <v-text-field
+      label="Email"
+      v-model="email"
+    ></v-text-field>
+    <v-text-field
+      label="Password"
+      v-model="password"
+      :append-icon="isShow ? 'visibility' : 'visibility_off'"
+              :append-icon-cb="() => (isShow = !isShow)"
+              :type="isShow ? 'text' : 'password'"
+              counter
+    ></v-text-field>
+
+    <v-btn @click="Login">LOGIN</v-btn>
+    <td><router-link to="regist">Create User</router-link></td>
+    <router-view></router-view>
+    </v-flex>
+    </v-layout>
+  </v-form>
+</template>
+
+<script>
+import {HTTP} from '@/router/index'
+export default{
+  data () {
+    return {
+      email: null,
+      password: null,
+      user_list: [],
+      isShow: false
+    }
+  },
+  methods: {
+    Login: function () {
+      var email = this.email
+      var pass = this.password
+    //   console.log('var: ', email, pass)
+      HTTP.get('/api/user').then(response => {
+        this.user_list = response.data.data
+        this.user_list.forEach(function (element) {
+        //   console.log('from db: ', element.email, element.password)
+          if (email === element.email && pass === element.password) {
+            console.log('Login Success!')
+            this.$router.push('mhsw')
+          }
+        }, this)
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
+    }
+  }
+}
+</script>
---
+++ vuetify_tamplate/webpack_advanced/src/components/register.vue
@@ -0,0 +1,72 @@
+<template>
+<v-form>
+    <v-layout justify-center>
+    <v-flex xs12 sm10 md8 lg6>
+          <v-text-field label="Full Name" v-model="name" required></v-text-field>
+          <v-text-field label="Email" v-model="email" placeholder="michel@gmail.com" required></v-text-field>
+          <v-text-field label="Password" v-model="password" required :append-icon="isShow ? 'visibility' : 'visibility_off'"
+              :append-icon-cb="() => (isShow = !isShow)"
+              :type="isShow ? 'text' : 'password'"
+              counter></v-text-field>
+          <v-select autocomplete label="Country" placeholder="Select..." :items="countries" v-model="country" ref="country"></v-select>
+          <v-btn v-on:click="submit">SUBMIT</v-btn>
+          <v-btn v-on:click="cancel">CANCEL</v-btn>
+    </v-flex>
+  </v-layout>
+</v-form>
+</template>
+
+<script>
+import {HTTP} from '@/router/index'
+export default {
+  data () {
+    return {
+      countries: ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor Leste', 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'],
+      name: null,
+      country: null,
+      email: null,
+      password: null,
+      isShow: false
+    }
+  },
+  methods: {
+    submit: function () {
+      console.log('submit')
+      var name = this.name
+      var email = this.email
+      var password = this.password
+      var countries = this.countries
+      var userJson = {
+        data: {
+          name,
+          email,
+          password,
+          countries
+        }
+      }
+      HTTP.post('/api/user', userJson)
+      .then(response => {
+        console.log('succes create user with id: ' + response.data.data.id)
+        this.rule()
+        this.$router.push('login')
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
+    },
+    cancel: function () {
+      this.name = ''
+      this.email = ''
+      this.password = ''
+      this.country = ''
+      this.$router.push('login')
+    },
+    rule: function () {
+      this.name = ''
+      this.email = ''
+      this.password = ''
+      this.country = ''
+    }
+  }
+}
+</script>
